d9f51dc0c0df21e794ec4c5136cf08d616d9f05e
Marchiz <agungxyz129@gmail.com>
2023-11-21 22:41:39 +0700

    Player, Camera, Level
    Penambahan Player, Camera, dan Script untuk Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-
+    [SerializeField] private Camera cam;
     private Rigidbody rb;
 
     private void Awake() 
     {
         rb = GetComponent<Rigidbody>();
+        HideAndLockCursor();
     }
-    // Update is called once per frame
+
+    // Method untuk hide cursor
+    private void HideAndLockCursor() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     void FixedUpdate()
     {
         // Input menentukan arah gerak (w,a,s,d)
         float horizontalMove = Input.GetAxis("Horizontal");
         float verticalMove = Input.GetAxis("Vertical");
 
+        // Script pergerakan agar mengikuti kamera
+        Vector3 horizontalDirection = horizontalMove * cam.transform.right;
+        Vector3 verticalDirection = verticalMove * cam.transform.forward;
+        horizontalDirection.y = 0;
+        verticalDirection.y = 0;
+
         // Menentukan move direction
-        Vector3 moveDirection = new Vector3(horizontalMove,0,verticalMove);
+        Vector3 moveDirection = horizontalDirection + verticalDirection;
 
         // Player movement
         rb.velocity = moveDirection * moveSpeed * Time.fixedDeltaTime;
